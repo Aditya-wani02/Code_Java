41823e07df963c79998849a42e4808e328029953
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-07-23 23:56:52 +0530

    Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,9 +1,8 @@
 
-
 public class Main {
 
     public static void main(String[] args) {
-   MyLinkedList list = new MyLinkedList();
+        MyLinkedList list = new MyLinkedList();
 
         list.insert(1);
         list.insert(2);
@@ -12,10 +11,13 @@ public class Main {
         list.insert(5);
         list.insert(6);
 
-        System.out.println(list.indexOf(1));
+        // System.out.println(list.indexOf(1));
+        // System.out.println(list.sum());
+        System.out.println((list));
+        list.insertInBegin(10);
+        list.insertInBegin(10);
+        System.out.println(list);
 
-
-        
     }
-    
+
 }

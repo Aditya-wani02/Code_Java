a076a67ce351f90e97fce8ba50408543ae24da1a
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-08 21:00:52 +0530

    Stack

--- Stack/Main.java
+++ Stack/Main.java
@@ -28,25 +28,3 @@ public class Main {
 
 }
 
-// MyLinkedList list = new MyLinkedList();
-
-// list.insert(1);
-// list.insert(2);
-// list.insert(3);
-// list.insert(4);
-// list.insert(5);
-// list.insert(6);
-
-// // System.out.println(list.indexOf(1));
-// // System.out.println(list.sum());
-// System.out.println(list);
-// // list.insertInBegin(10);
-// // list.insertInBegin(10);
-// // list.insertAt(2, 23);
-// // list.insertAt(8, 99);
-// // list.reverse();
-// // list.deleteFirst();
-// // list.deletelast();
-// // list.deletelast();
-// list.removeNthFromEndfast(2);
-// System.out.println(list);
